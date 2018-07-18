16e84ee4cea3ece44f79413d399fad7bb7726e21
Deryk w. King <deryk.king@brafton.com>
2018-07-18 14:29:31 -0400

    re organize project to match brafton struct

---
+++ src/Todo.Shared/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TodoApi.Shared.Models{
+    public class TodoItem{
+        public long Id {get;set;}
+        public string Name {get;set;}
+        public bool IsComplete {get;set;}
+    }
+}
