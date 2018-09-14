f95042800db89e70dc90d7cda9d83f196a3106ff
Deryk w. King <deryk.king@brafton.com>
2018-09-14 16:57:15 -0400

    start adding automapper

---
+++ src/Todo.server/AutoMapperConfiguration/TodoItemMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using TodoApi.Shared.Models;
+
+namespace Todo.server.AutoMapperConfiguration
+{
+    public class TodoItemMappingProfile : Profile
+    {
+        public TodoItemMappingProfile()
+        {
+            this.CreateMap<TodoItemPrivate, TodoItemPublic>()
+                //.ForMember( dest=>dest.User, opt=>opt.MapFrom(src=>src.) )
+        }
+
+    }
+}
