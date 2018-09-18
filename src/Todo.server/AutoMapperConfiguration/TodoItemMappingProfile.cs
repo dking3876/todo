589629c04476c58599ef281bd8c0f6400c810f7f
Deryk w. King <deryk.king@brafton.com>
2018-09-18 17:06:22 -0400

    finish profile for private to public

--- src/Todo.server/AutoMapperConfiguration/TodoItemMappingProfile.cs
+++ src/Todo.server/AutoMapperConfiguration/TodoItemMappingProfile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
+using TodoApi.Shared;
 using TodoApi.Shared.Models;
 
 namespace Todo.server.AutoMapperConfiguration
@@ -11,7 +12,13 @@ namespace Todo.server.AutoMapperConfiguration
         public TodoItemMappingProfile()
         {
             this.CreateMap<TodoItemPrivate, TodoItemPublic>()
-                //.ForMember( dest=>dest.User, opt=>opt.MapFrom(src=>src.) )
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.UserId));
+
+            this.CreateMap<int, User>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src));
+
+
+
         }
 
     }
